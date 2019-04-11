c8772dcf25bdaba32baf5dbf938ef19cb227db01
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2019-04-11 12:55:45 -0300

    adicionando controlador de testes

---
+++ software/web/public/app/application/controllers/Testes.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+/* Arquivo para realizar testes */
+class Testes extends CI_Controller {
+
+    public function index(){}
+
+    /* Criar funções de testes abaixo ... */
+
+
+
+}
