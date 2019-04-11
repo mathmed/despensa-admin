a7f9266de084005ef02c81f5c1a92576d8c3c3e4
laioAndrade <laioand98@gmail.com>
2019-04-11 16:07:51 -0300

    Adicionando construtores

--- software/web/public/app/application/business/Compra_business.php
+++ software/web/public/app/application/business/Compra_business.php
@@ -3,7 +3,9 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Compra_business extends CI_Business {
-
+    public function __construct(){
+        parent::__construct();
+    }
  
 }
 
