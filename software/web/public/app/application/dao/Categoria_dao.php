e87436a1092d398b5c408336bbfdd7f6f3c50f8d
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2019-04-16 08:41:32 -0300

    adicionando estrutura de arquivos DAO

---
+++ software/web/public/app/application/dao/Categoria_dao.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Categoria_dao extends MY_Dao {
+
+    /* Construtor da classe */
+	public function __construct(){
+        parent::__construct();     
+    }
+}
