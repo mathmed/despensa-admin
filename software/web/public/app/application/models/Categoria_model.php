8e880ac3d63983906b805411cb9ebcf0c87b0d44
laioAndrade <laioand98@gmail.com>
2019-06-06 15:32:14 -0300

    retirando atributo excluido do BD

--- software/web/public/app/application/models/Categoria_model.php
+++ software/web/public/app/application/models/Categoria_model.php
@@ -6,13 +6,11 @@ class Categoria_model extends CI_Model{
     public $uid;
     public $descricao;
     public $validade_media;
-    public $tipo_medida;
 
     public function __construct($dados = NULL){
         $this->uid = isset($dados["uid"]) ? $dados["uid"]: NULL;
         $this->descricao = isset($dados["descricao"]) ? $dados["descricao"]: NULL;
         $this->validade_media = isset($dados["validade_media"]) ? $dados["validade_media"]: NULL;
-        $this->uid = isset($dados["tipo_medida"]) ? $dados["tipo_medida"]: NULL;
 
     }
 
@@ -40,15 +38,6 @@ class Categoria_model extends CI_Model{
         $this->validade_media = $validade_media;
     }
 
-    public function get_tipo_medida(){
-        return $this->tipo_medida;
-    }
 
-    public function set_tipo_medida($tipo_medida = NULL){
-        $this->tipo_medida = $tipo_medida;
-    }
-
-
-    
 
 }
